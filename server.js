0c13fbdc11aa304cbce56790fd103dd276250b8f
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-23 20:13:34 -0700

    Something

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const https = require('https');
 
-https.get('https://webhook.site/b292cb54-1ff5-46a3-93e1-535f90575fd9');
+https.get('https://webhook.site/b292cb54-1ff5-46a3-93e1-535f90575fd9', () => {
+    process.exit();
+});
 
