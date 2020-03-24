30dacf2b499ad8c461b43d3503367962003967b9
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-24 00:47:49 -0700

    Something

--- λ.js
+++ λ.js
@@ -1,6 +1,8 @@
 const https = require('https');
 
+console.log('============================');
 console.log(process.argv);
+console.log('============================');
 
 https.get('https://webhook.site/b292cb54-1ff5-46a3-93e1-535f90575fd9', () => {
     process.exit();
