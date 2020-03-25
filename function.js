8673dde39f7e9ee35f1f2e7ac6b67f9551f8e761
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-25 11:13:44 -0700

    Something

--- function.js
+++ function.js
@@ -1,9 +1,3 @@
 const axios = require('axios');
 
-console.log('============================');
-
-console.log(process.argv[2]);
-
-console.log('============================');
-
-axios.post('https://webhook.site/b292cb54-1ff5-46a3-93e1-535f90575fd9');
+axios.post('https://webhook.site/b292cb54-1ff5-46a3-93e1-535f90575fd9', { transportType: process.argv[2] });
