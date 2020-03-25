0541ed7ac6d3d6bb31aeaaf9d9ef94b50340d308
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-25 07:52:45 -0700

    Something

---
+++ function_invoker.js
@@ -0,0 +1,84 @@
+process.env.DATABASE_URL = 'postgres://u8f6tki4kd7t7o:p6e81aa2b5e1c738bfc56a0068949fbfc1bd73aefd2c93e30eaf74793568160ca@ec2-3-227-37-147.compute-1.amazonaws.com:5432/d619mlqhcpeng3';
+process.env.DATABASE_URL += '?ssl=true';
+
+const axios = require('axios');
+const { Client } = require('pg');
+const express = require('express');
+const jsforce = require('jsforce');
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const conn = new jsforce.Connection({});
+
+const client = new Client({
+    ssl: {
+        rejectUnauthorized: true,
+    },
+    connectionString: process.env.DATABASE_URL
+});
+
+const postData = {
+    attach: false,
+	size: 'standard-1X',
+	type: 'run',
+	time_to_live: 1800
+};
+const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.heroku+json; version=3',
+    'Authorization': 'Bearer 8c2f5164-1d2f-4c23-9399-3fd9cc0f706b'
+};
+
+async function runFunction(data) {
+    postData.command = `npm run function`;
+    await axios.post('https://api.heroku.com/apps/intel-faas/dynos', postData, { headers });
+}
+
+(async () => {
+    await conn.login('sshaginyan@demo.com', '474925~Ste' + 'qi8O5SrMmrEooKYy7KrmuLifG');
+    //await conn.login('steve@myfirstdemo.demo', 'abc123abc');
+
+    conn.streaming.subscribe('/data/ChangeEvents', data => {
+        runFunction({ transportType: 'Change Data Capture', data });
+    });
+    
+    conn.streaming.subscribe('/event/PlatformEvents__e', data => {
+        runFunction({ transportType: 'Platform Events', data });
+    });
+
+})();
+
+client.connect((error, client) => {
+    if(error) throw error;
+
+    client.on('notification', msg => {
+        const data = JSON.parse(msg.payload);
+        runFunction({ transportType: 'Heroku Connect', data });
+    });
+    client.query('LISTEN faas');
+});
+
+app.post('/http', (request, response) => {
+    runFunction({ transportType: 'HTTP', data: request.body });
+    response.sendStatus(200);
+});
+
+app.post('/apex', (request, response) => {
+    runFunction({ transportType: 'APEX', data: request.body });
+    response.sendStatus(200);
+});
+
+app.post('/lightning_web_components', (request, response) => {
+    runFunction({ transportType: 'Lightning Web Components', data: request.body });
+    response.sendStatus(200);
+});
+
+app.post('/flow_builder', (request, response) => {
+    runFunction({ transportType: 'Flow Builder', data: request.body });
+    response.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 8080);
